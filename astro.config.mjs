0be0125fe21eb616e0acb31dce6b24dbd9fede0d
Dorell James <galangdj@gmail.com>
2024-06-02 15:49:45 +0800

    chore: revert gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,11 @@ import { defineConfig, passthroughImageService } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import rehypeExternalLinks from 'rehype-external-links'
 import sitemap from '@astrojs/sitemap'
-import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.dorelljames.com',
-	integrations: [
-		tailwind(),
-		sitemap(),
-		partytown({
-			config: {
-				forward: ['dataLayer.push']
-			}
-		})
-	],
+	integrations: [tailwind(), sitemap()],
 	image: {
 		service: passthroughImageService()
 	},
